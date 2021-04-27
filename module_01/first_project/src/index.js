8d69dd3d5b5fd86553f47d9edd87ecc090a5c311
Ariel Kollross <arielkollross@gmail.com>
2021-04-26 23:59:51 -0300

    add balance, deposit and debit

--- module_01/first_project/src/index.js
+++ module_01/first_project/src/index.js
@@ -16,13 +16,39 @@ function verifyExistsAccountDocument(request, response, next) {
     return response.status(400).json({ "error": "Customer not found"});
   }
 
+  request.customer = getCustomerByDocument;
+
   return next();
+}
+
+function getBalance(customer) {
+  if(!customer || !customer.statement) {
+    return response.status(422).json({ "error": "request can not be precessed"})
+  }
+
+  const balance = {
+    amount: 0,
+    debit: 0,
+    balance: 0,
+  }
+
+  for (let record in customer.statement) {
+    if(record.type === "credit") {
+      balance.amount += record.amount
+    } else {
+      balance.debit += record.amount 
+    }
+  }
+
+  balance.balance = balance.amount - balance.debit
+
+  return balance
 }
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
-  // simple way to check, but may not be efficient
+  // simple way to check
   const customerAlreadyExists = customers.some(
     (customer) => customer.cpf === cpf 
   );
@@ -33,23 +59,68 @@ app.post("/account", (request, response) => {
     });
   }
 
-  customers.push({ 
+  const customer = { 
     id: uuidv4(),
     name,
     cpf, 
     statement: [] 
-  });
+  }
 
-  return response.status(201).send();
-})
+  customers.push(customer);
+
+  return response.status(201).json({ customer });
+});
 
 app.get("/account", (request, response) => {
   return response.status(200).json({customers});
-})
+});
 
 // app.use(verifyExistsAccountDocument) // use in all routes 
 app.get("/statement/", verifyExistsAccountDocument, (request, response) => {
-  return response.status(200).json(getCustomerByDocument.statement);
+  const { customer } = request;
+
+  return response.status(200).json(customer.statement);
+});
+
+app.post('/deposit', verifyExistsAccountDocument, (request, response) => {
+  const { description, amount } = request.body;
+
+  if( !description || !amount ) {
+    return response.status(400).json({ 
+      "error": "fields description/amount are mandatory"
+    });
+  }
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    type: 'credit',
+    created_at: new Date(),
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).json({ customer });
+
+}); 
+
+app.post('/withdrawal', verifyExistsAccountDocument, (request, response) => {
+  const { description, amount } = request.body;
+
+  if( !description || amount ) {
+    return response.status(400).json({ 
+      "error": "fields description/amount are mandatory"
+    });
+  } 
+
+  const { customer } = request;
+
+  const balance =  getBalance(customer);
+
+  return response.status(201).json({ balance });
+
 });
 
 app.listen(3333);
